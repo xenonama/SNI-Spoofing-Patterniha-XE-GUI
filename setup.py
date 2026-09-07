250e9df6d086853f345e08814804c2868a0ed3e3
xenon <itsxenonmc@gmail.com>
2026-09-07 09:26:05 +0330

    Add setup.py for Python package configuration

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SNI-Spoofing-Patterniha",
+    version="0.1.0",
+    description="ابزار برای پترنیها هستش. یکم تغییرش دادم با دوست خوبم ای آی (GUI اضافه کردم بهش)",
+    author="xenonama",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    license="MIT",
+)
